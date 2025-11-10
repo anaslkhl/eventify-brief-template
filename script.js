6deb802b381d7eedadb3c8ffbd3d7988a03710c7
anaslkhl <anaslkhl123@gmail.com>
2025-11-10 09:19:59 +0100

    finished the party of adding an event

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let events = [];
+
+
+
+
+
 let archive = [];
 let btnstats = document.querySelectorAll('.sidebar__btn')
 let screen = document.querySelectorAll(".screen");
 
-let form = document.getElementsByClassName("form")[0]
 
-console.log(form);
+//the function of switching between screens//
 
-
-function switch_screen (key) {
+function switch_screen(key) {
     const targetone = key.dataset.screen
     screen.forEach(ev => {
         ev.classList.remove("is-visible")
-        if (ev.dataset.screen === targetone){
+        if (ev.dataset.screen === targetone) {
             ev.classList.add("is-visible")
         }
         btnstats.forEach(btn => {
@@ -22,29 +24,82 @@ function switch_screen (key) {
     });
 }
 
-form.addEventListener("submit", (e)=> {
-    e.preventDefault()
-    inputsvalue()
-    
-})
 
-function inputsvalue() {
+//Add an event for adding the events//
 
-const eventTittle = document.getElementById("event-title")
-const eventImage = document.getElementById("event-image")
-const eventDescription = document.getElementById("event-description")
-const eventSeats = document.getElementById("event-seats")
-const eventPrice = document.getElementById("event-price")
+const form = document.querySelector(".form")
+const tableBody = document.querySelector(".table__body")
 
-const eventTittlev = eventTittle.value
-const eventImagev = eventImage.value
-const eventDescriptionv = eventDescription.value
-const eventSeatsv = eventSeats.value
-const eventPricev = eventPrice.value
+// make data stores on localstorage //
 
-events.push({eventTittlev, eventImagev, eventSeatsv, eventPricev, eventDescriptionv})
-form.reset()
+const events = JSON.parse(localStorage.getItem('events')) || [];
+let nextId = events.length ? events[events.length - 1].id + 1 : 1;
+
+function TableShow() {
+
+    tableBody.innerHTML = '';
+    events.forEach(ev => {
+        const tr = document.createElement('tr')
+        tr.className = 'table__row'
+        tr.dataset.evenId = ev.id;
+        tr.innerHTML = `
+                        <td>${ev.id}</td>
+                        <td>${ev.title}</td>
+                        <td>${ev.seat}</td>
+                        <td>${ev.price}$</td>
+                        <td><img src="${ev.image}" alt="event pic" style="width:60px; height:auto; object-fit:cover;"></td>
+                        <td><span class="badge" id="eventVariant">3</span></td>
+                        <td>
+                            <button class="btn btn--small" data-action="details"
+                                data-event-id="${ev.id}">Details</button>
+                            <button class="btn btn--small"data-action="edit"
+                                data-event-id="${ev.id}">Edit</button>
+                            <button class="btn btn--danger btn--small" data-action="archive"
+                                data-event-id="${ev.id}">Delete</button>
+                        </td>
+                                    `;
+
+        tableBody.appendChild(tr)
+    })
 }
+TableShow();
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    console.log('commed')
+    const evenTittle = document.querySelector('#event-title').value.trim()
+    const eventImage = document.querySelector('#event-image').value.trim()
+    const eventDescription = document.querySelector('#event-description').value.trim()
+    const eventSeats = document.querySelector('#event-seats').value.trim()
+    const eventPrice = document.querySelector('#event-price').value.trim()
+    const NewEvent =
+    {
+        id: nextId,
+        title: evenTittle,
+        image: eventImage,
+        description: eventDescription,
+        seat: eventSeats,
+        price: eventPrice
+    }
+
+    events.push(NewEvent);
+    localStorage.setItem('events', JSON.stringify(events))
+    TableShow()
+    nextId++
+    form.reset()
+});
+
+
+
+function ToArchive() {
+    
+}
+
+
+
+
+
+
 
 
 
